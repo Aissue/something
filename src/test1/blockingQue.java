cd9a69010214b9fca33e6bd219e1a91ddcfccc19
Administrator <aissue@126.com>
2017-11-05 11:22:52 +0800

    Initial commit

---
+++ src/test1/blockingQue.java
@@ -0,0 +1,42 @@
+package test1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * 阻塞队列测试
+ * Created by Administrator on 2017/9/28.
+ */
+public class blockingQue {
+    private BlockingQueue<String> blockingQueue;
+
+    @Before
+    public void beforeSet(){
+//        this.blockingQueue = new LinkedBlockingQueue<>(10);
+        this.blockingQueue = new ArrayBlockingQueue<>(2);
+
+    }
+
+    @Test
+    public void test1(){
+        for(int i=0;i<3;i++){
+            /*add(e)添加元素队列满了的话会报错java.lang.IllegalStateException: Queue full*/
+//            blockingQueue.add("test"+i);
+            /*offer(e)添加元素队列满了的话会返回false，不会报错*/
+//            System.out.println(blockingQueue.offer("test"+i));
+            try {
+                /*当加入第三个元素时线程阻塞，要等到有足够空间才自动唤醒*/
+                blockingQueue.put("test"+i);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("");
+
+    }
+
+}
---
+++ src/test1/delayEntity.java
@@ -0,0 +1,58 @@
+package test1;
+
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Administrator on 2017/11/5.
+ */
+public class delayEntity implements Delayed {
+
+    /*时间单位*/
+    private TimeUnit timeUnit;
+    /*具体的时间参数*/
+    private long liveTime;
+
+    private long removeTime;
+
+    private String name;
+
+    public delayEntity(String name , TimeUnit tu, long lt){
+        this.name = name;
+        this.timeUnit = tu;
+        this.liveTime = lt;
+        this.removeTime = TimeUnit.NANOSECONDS.convert(liveTime, tu) + System.nanoTime();
+    }
+
+    @Override
+    public long getDelay(TimeUnit unit) {
+        System.out.println("getDelay_method has run");
+        return unit.convert(removeTime - System.nanoTime(), unit);
+    }
+
+    @Override
+    public int compareTo(Delayed o) {
+        System.out.println("compareTo_method has run");
+        if (o == null) return 1;
+        if (o == this) return 0;
+        if (o instanceof delayEntity){
+            delayEntity tmpDelayedItem = (delayEntity)o;
+            if (liveTime > tmpDelayedItem.liveTime ) {
+                return 1;
+            }else if (liveTime == tmpDelayedItem.liveTime) {
+                return 0;
+            }else {
+                return -1;
+            }
+        }
+        long diff = getDelay(TimeUnit.NANOSECONDS) - o.getDelay(TimeUnit.NANOSECONDS);
+        return diff > 0 ? 1:diff == 0? 0:-1;
+    }
+
+    @Override
+    public String toString() {
+        return "delayEntity{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
